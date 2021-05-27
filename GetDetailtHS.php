86ecb7dbebf1a3b0b1826137d89490ad9183b008
TuanNguyen <tuannguyenipv6@gmail.com>
2021-05-27 13:07:48 +0700

    xóa hs

--- GetDetailtHS.php
+++ GetDetailtHS.php
@@ -1,59 +1,69 @@
 <?php
     require "truyvanData.php";
     $mID  =       $_POST['pID'];
-    $Check = 0;
-    $TruyVan = "SELECT * FROM hocsinh WHERE FIND_IN_SET('$mID', dID)";
-    //chứa dữ liệu vào 1 biến
-    $data = mysqli_query($KetNoi, $TruyVan);
-    if($data){
-        while($dong = mysqli_fetch_assoc($data)){
-            if($dong["dGioiTinh"] == 1){
-                $GT = true;
-            }else $GT = false;
-            $result = array("ID" =>$dong["dID"], 
-                            "HoTen" => $dong["dHoTen"],
-                            "MSHS" => $dong["dMSHS"],
-                            "NamSinh" => $dong["dNamSinh"],
-                            "GioiTinh" => $GT,
-                            "DanToc" => $dong["dDanToc"],
-                            "NoiSinh" => $dong["dNoiSinh"],
-                            "ChucVu" => $dong["dChucVu"],
-                            "SdtPh" => $dong["dSdtPh"],
-                            "LinkPhoto" => $dong["dLinkPhoto"],
-                            "Linkfb" => $dong["dLinkfb"],
-                            "GhiChu" => $dong["dGhiChu"]
-                        );
-                        $Check = 1;
-        }
+    $mCheck  =       $_POST['pCheck'];
+
+    if($mCheck == 1){
+        $Delete = "DELETE FROM hocsinh WHERE dID = '$mID'";
+        $data = mysqli_query($KetNoi, $Delete);
+        if($data){
+            $result = array("HoTen" => "Success");
+        }else $result = array("HoTen" => "Error");
     }else{
-        $result = array("ID" => -1, 
-                            "HoTen" => "Lỗi Truy Vấn!!!",
-                            "MSHS" => 0,
-                            "NamSinh" => 0,
-                            "GioiTinh" => true,
-                            "DanToc" => "Error",
-                            "NoiSinh" => "Error",
-                            "ChucVu" => "Error",
-                            "SdtPh" => "Error",
-                            "LinkPhoto" => "ErrorPhoto",
-                            "Linkfb" => "ErrorFB",
-                            "GhiChu" => "Error"
-                        );
-    }
-    if($Check == 0){
-        $result = array("ID" => -1, 
-                            "HoTen" => "Lỗi Truy Vấn!!!",
-                            "MSHS" => 0,
-                            "NamSinh" => 0,
-                            "GioiTinh" => true,
-                            "DanToc" => "Error",
-                            "NoiSinh" => "Error",
-                            "ChucVu" => "Error",
-                            "SdtPh" => "Error",
-                            "LinkPhoto" => "ErrorPhoto",
-                            "Linkfb" => "ErrorFB",
-                            "GhiChu" => "Error"
-                        );
+        $Check = 0;
+        $TruyVan = "SELECT * FROM hocsinh WHERE FIND_IN_SET('$mID', dID)";
+        //chứa dữ liệu vào 1 biến
+        $data = mysqli_query($KetNoi, $TruyVan);
+        if($data){
+            while($dong = mysqli_fetch_assoc($data)){
+                if($dong["dGioiTinh"] == 1){
+                    $GT = true;
+                }else $GT = false;
+                $result = array("ID" =>$dong["dID"], 
+                                "HoTen" => $dong["dHoTen"],
+                                "MSHS" => $dong["dMSHS"],
+                                "NamSinh" => $dong["dNamSinh"],
+                                "GioiTinh" => $GT,
+                                "DanToc" => $dong["dDanToc"],
+                                "NoiSinh" => $dong["dNoiSinh"],
+                                "ChucVu" => $dong["dChucVu"],
+                                "SdtPh" => $dong["dSdtPh"],
+                                "LinkPhoto" => $dong["dLinkPhoto"],
+                                "Linkfb" => $dong["dLinkfb"],
+                                "GhiChu" => $dong["dGhiChu"]
+                            );
+                            $Check = 1;
+            }
+        }else{
+            $result = array("ID" => -1, 
+                                "HoTen" => "Lỗi Truy Vấn!!!",
+                                "MSHS" => 0,
+                                "NamSinh" => 0,
+                                "GioiTinh" => true,
+                                "DanToc" => "Error",
+                                "NoiSinh" => "Error",
+                                "ChucVu" => "Error",
+                                "SdtPh" => "Error",
+                                "LinkPhoto" => "ErrorPhoto",
+                                "Linkfb" => "ErrorFB",
+                                "GhiChu" => "Error"
+                            );
+        }
+        if($Check == 0){
+            $result = array("ID" => -1, 
+                                "HoTen" => "Lỗi Truy Vấn!!!",
+                                "MSHS" => 0,
+                                "NamSinh" => 0,
+                                "GioiTinh" => true,
+                                "DanToc" => "Error",
+                                "NoiSinh" => "Error",
+                                "ChucVu" => "Error",
+                                "SdtPh" => "Error",
+                                "LinkPhoto" => "ErrorPhoto",
+                                "Linkfb" => "ErrorFB",
+                                "GhiChu" => "Error"
+                            );
+        }
     }
     echo json_encode($result);
 ?>
